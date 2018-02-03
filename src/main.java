86283acb0cb1545bdee3743f61dedcacdee49871
AlexGenio <rob_b-boy_seb@hotmail.com>
2018-02-03 13:42:01 -0500

    Created package structure for exercise 1 and put in a main.

--- src/main.java
+++
@@ -1,9 +0,0 @@
-/*
- * SOEN 487 - Winter 2018
- * 
- */
-
-
-public class main {
-
-}
---
+++ src/pm1/xmlexercise/XMLParser.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pm1.xmlexercise;
+
+/**
+ * @author AlexGenio
+ *
+ */
+public class XMLParser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
