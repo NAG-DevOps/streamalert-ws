176dc4107d1429aae08eeb8cd563422ffff8e938
Robert McAuley <mcauley.robert@live.com>
2018-02-11 23:29:20 -0500

    added home servlet

---
+++ tomcat/StreamAlert/src/Home.java
@@ -0,0 +1,34 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Home() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+        request.getRequestDispatcher("/WEB-INF/jsp/home.jsp").forward(request, response);
+	}
+
+}
