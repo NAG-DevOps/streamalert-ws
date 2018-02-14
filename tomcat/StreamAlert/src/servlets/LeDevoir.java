b7afe06767ee16f1a74fa410cf54c096d25ff66e
Progaine <alidouch@hotmail.com>
2018-02-14 02:57:39 -0500

    Process RSS XML feed from LeDevoir (#5)
    The content of all "title" elements from Le Devoir's RSS feed is
    displayed at /le_devoir.

---
+++ tomcat/StreamAlert/src/servlets/LeDevoir.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+/**
+ * Servlet implementation class LeDevoir
+ */
+@WebServlet("/le_devoir")
+public class LeDevoir extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LeDevoir() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			ArrayList<String> parsedStrings = DOMParser.parse("http://www.ledevoir.com/rss/edition_complete.xml", "title");
+			request.setAttribute("parsedStrings", parsedStrings);
+		} catch (ParserConfigurationException | IOException| SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.getRequestDispatcher("WEB-INF/jsp/le_devoir.jsp").forward(request, response);
+	}
+}
