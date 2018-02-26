7988395913ee64b44294031fe017fd375cb08f0d
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2018-02-25 19:01:44 -0500

    #13 enable S3 file upload events

--- streamalert/rules/community/xml/xml_request_received.py
+++ streamalert/rules/community/xml/xml_request_received.py
@@ -2,8 +2,7 @@
 from stream_alert.rule_processor.rules_engine import StreamRules
 rule = StreamRules.rule
 
-
-@rule(logs=['test-data'],               # applicable datasource(s)
-      outputs=['slack:soen487'])   # where to send alerts
-def send_xml_to_slack(record):                  # the rule name will be 'example'
+@rule(logs=['test-data'],
+      outputs=['slack:soen487'])
+def send_xml_to_slack(record):
     return record
