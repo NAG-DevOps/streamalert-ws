9066552295408ace93ea54d154c6ef5f0ab6f45d
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2018-03-13 20:00:54 -0400

    update stream alert schema for neural network (#12)

--- streamalert/rules/community/xml/xml_request_received.py
+++ streamalert/rules/community/xml/xml_request_received.py
@@ -2,12 +2,6 @@
 from stream_alert.rule_processor.rules_engine import StreamRules
 rule = StreamRules.rule
 
-@rule(logs=['test-data'],
-      outputs=['slack:soen487'])
-def send_xml_to_slack(record):
-    return record
-
-@rule(logs=['neural-network', 'neural-network:events'],
-      outputs=['slack:soen487'])
+@rule(logs=['neural-network'], outputs=['slack:soen487'])
 def send_nn_to_slack(record):
-    return record
+    return True
