31b611d5b290b568a0abac8d401ae6cecac7143d
Joshua <joshuachanfee@gmail.com>
2018-03-15 17:39:53 -0400

    augmented soap code to include github feed

--- tomcat/StreamAlert/src/utils/Requests.java
+++ tomcat/StreamAlert/src/utils/Requests.java
@@ -1,8 +1,11 @@
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class Requests {
 	
@@ -24,6 +27,33 @@ public class Requests {
 			result.append(line);
 		}
 		rd.close();
+		return result.toString();
+	}
+	
+	/**
+	 * Request a HTTP GET method on a URL.
+	 * @param urlToRead URL to open the connection on
+	 * @return Formatted string containing the request's input stream
+	 * @throws Exception
+	 */
+	public static String getWithBasicAuth(String urlToRead, String username, String password) throws Exception {
+		URL url = new URL (urlToRead);
+        //String encoding = Base64.getEncoder().encodeToString((username+":"+password).getBytes(‌"utf-8"));
+		String authStr = username + ":" + password;
+//		String encoding = Base64.getEncoder().encodeToString(Base64.getEncoder().encode(authStr.getBytes()));
+		String encoding = Base64.getEncoder().encodeToString((username+":"+password).getBytes(StandardCharsets.UTF_8));
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Authorization", "Basic " + encoding);
+        InputStream content = (InputStream)connection.getInputStream();
+        BufferedReader in = new BufferedReader (new InputStreamReader (content));
+        String line;
+        StringBuilder result = new StringBuilder();
+        while ((line = in.readLine()) != null) {
+            result.append(line);
+        }
+		in.close();
 		return result.toString();
 	}
 }
