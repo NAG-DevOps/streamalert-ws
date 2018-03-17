9f3ff49dc70d024430d44ed25c9d9807a4e418e8
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2018-03-17 13:09:26 -0400

    reorganize logs and schemas (#21)

--- streamalert/rules/community/github/github_get_feeds.py
+++ streamalert/rules/community/github/github_get_feeds.py
@@ -2,6 +2,6 @@
 from stream_alert.rule_processor.rules_engine import StreamRules
 rule = StreamRules.rule
 
-@rule(logs=['feeds'], outputs=['slack:soen487'])
-def send_github_feeds_to_slack(record):
+@rule(logs=['github_feeds'], outputs=['slack:soen487'])
+def github_get_feeds(record):
     return True
--- streamalert/rules/community/github/github_get_single_issue.py
+++ streamalert/rules/community/github/github_get_single_issue.py
@@ -2,6 +2,6 @@
 from stream_alert.rule_processor.rules_engine import StreamRules
 rule = StreamRules.rule
 
-@rule(logs=['issue'], outputs=['slack:soen487'], )
-def github_issue_retrieved(record):
+@rule(logs=['github_issue'], outputs=['slack:soen487'], )
+def github_get_single_issue(record):
     return True
