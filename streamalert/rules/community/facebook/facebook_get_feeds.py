08b356a51e05dd8c7b3da445872f5f37b8e922ab
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2018-03-17 13:48:47 -0400

    define rules and their local tests for facebook feeds (#21)

---
+++ streamalert/rules/community/facebook/facebook_get_feeds.py
@@ -0,0 +1,8 @@
+"""Alert on an incoming SOAP request for GitHub."""
+from stream_alert.rule_processor.rules_engine import StreamRules
+rule = StreamRules.rule
+
+@rule(logs=['facebook_feeds'], outputs=['slack:soen487'])
+def facebook_get_feeds(record):
+    return True
+    
