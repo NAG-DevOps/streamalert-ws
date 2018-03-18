3a54b6095a44f3774db9038a345228eb667ddde4
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2018-03-18 19:34:24 -0400

    define rules and their local tests for wsc-gen feeds (#21)

--- streamalert/rules/community/soen487_ws/teams.py
+++ streamalert/rules/community/soen487_ws/teams.py
@@ -4,4 +4,8 @@ rule = StreamRules.rule
 
 @rule(logs=['splints_feeds'], outputs=['slack:soen487'], )
 def get_latest_splints_feed(record):
-    return True
+    return record['repo']['name'] == 'NAG-DevOps/splints'
+
+@rule(logs=['wscgen_feeds'], outputs=['slack:soen487'], )
+def get_latest_wscgen_feed(record):
+    return record['repo']['name'] == 'GIPSY-dev/WSC-Gen'
